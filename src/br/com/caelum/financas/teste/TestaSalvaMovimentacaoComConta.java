2b6af564427769ee217fcba608dfc8d9d7d73834
Luis Amaral <lgaa2006@gmail.com>
2015-05-02 11:29:35 -0300

    Testing the insertion of a transaction with a defined account into the DB. Won't work b/c account's TRANSIENT mode!

---
+++ src/br/com/caelum/financas/teste/TestaSalvaMovimentacaoComConta.java
@@ -0,0 +1,40 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TestaSalvaMovimentacaoComConta {
+	public static void main(String[] args) {
+		EntityManager entityManager = new JPAUtil().getEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Conta conta = new Conta();
+		
+		conta.setBanco("Santander");
+		conta.setNumero("9999909");
+		conta.setAgencia("999");
+		conta.setTitular("Maria da Silva");
+		
+		Movimentacao movimentacao = new Movimentacao();
+		
+		movimentacao.setConta(conta);
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setDescricao("Conta de Luz - Maio/15");
+		movimentacao.setValor(new BigDecimal("125"));
+		movimentacao.setTipoMovimentacao(TipoMovimentacao.SAIDA);
+	
+		entityManager.persist(movimentacao);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+		System.out.println("Movimentacao adicionada com sucesso!");
+	}
+}
