770392e5e4d8f1c6a48d4ee38cd836f640ca121c
Luis Amaral <lgaa2006@gmail.com>
2015-05-02 11:01:42 -0300

    Testing all methods from DAO

--- src/br/com/caelum/financas/teste/TestaRemoveConta.java
+++ src/br/com/caelum/financas/teste/TestaRemoveConta.java
@@ -6,27 +6,19 @@ import br.com.caelum.financas.dao.ContaDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.util.JPAUtil;
 
-public class TestaInsereConta {
+public class TestaRemoveConta {
 	public static void main(String[] args) {
-		
-		EntityManager entityManager = new JPAUtil().getEntityManager(); 
-		
+		EntityManager entityManager = new JPAUtil().getEntityManager();
 		ContaDao dao = new ContaDao(entityManager);
 		
-		Conta conta = new Conta();
-		
-		conta.setTitular("Luis Gabriel");
-		conta.setBanco("Banco do Brasil");
-		conta.setNumero("54321-6");
-		conta.setAgencia("0111");
-		
 		entityManager.getTransaction().begin();
-
-		dao.adiciona(conta);
+		
+		Conta conta = dao.busca(1);
+		dao.remove(conta);
 		
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		
-		System.out.println("Conta gravada com sucesso!");
+		System.out.println("Conta removida com sucesso!");
 	}
 }
